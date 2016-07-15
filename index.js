9e624226972cbeed3730574ec1f046a9869a9c1e
Dan Burzo <danburzo@gmail.com>
2016-07-15 18:45:17 +0300

    Clean up the code a bit, add comments, fix bug for empty Reading List

--- index.js
+++ index.js
@@ -2,8 +2,42 @@ var plist = require('simple-plist');
 var fs = require('fs-extra');
 var path = require('path');
 
+// This is where Safari keeps its bookmark file
 var READING_LIST_FILE = path.join(process.env.HOME, '/Library/Safari/Bookmarks.plist');
 
+// Narrow down to the Reading List items from Safari's bookmarks file
+function getReadingList(data) {
+	var reading_list = data.Children.find(function(item) {
+		return item.Title === 'com.apple.ReadingList';
+	});
+	return reading_list && reading_list.Children ? reading_list.Children : [];
+};
+
+// For each item in the Reading List, extract data into a saner JSON structure. 
+function parseBookmark(bookmark) {
+	return {
+		href: bookmark.URLString,
+		title: bookmark.ReadingListNonSync.Title || bookmark.URIDictionary.title,
+		time: bookmark.ReadingList.DateAdded,
+		description: bookmark.ReadingList.PreviewText || ''
+	}
+};
+
+// Format the JSON object for the bookmark item into a HTML fragment
+function item_formatter(item) {
+
+	// transform the standard timestamp to `seconds-from-1970` timestamp used in Netscape format
+	var timestamp = (new Date(item.time).getTime() + '').replace(/\d{3}$/, '');
+
+	// sanitize the HTML description so it does not break the markup (further)
+	var description = sanitize(item.description);
+
+	return `<DT><A HREF="${item.href}" ADD_DATE="${timestamp}">${item.title}</A>
+		<DD>${description}
+	`;
+};
+
+// Wrap the HTML fragments for the bookmark items in the Netscape Bookmark File Format.
 function netscape_template(content) {
 	return `<!DOCTYPE NETSCAPE-Bookmark-file-1>
 <!-- This is an automatically generated file.
@@ -17,35 +51,19 @@ ${content}
 </DL><p>`;
 };
 
+// Generic method to sanitize a HTML string
 function sanitize(str) {
-	return (str+"").replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"&quot;").replace(/'/g,"&#x27;")
+	return (str + "")
+		.replace(/&/g,"&amp;")
+		.replace(/</g,"&lt;")
+		.replace(/>/g,"&gt;")
+		.replace(/"/g,"&quot;")
+		.replace(/'/g,"&#x27;")
 };
 
-function item_formatter(item) {
-	var timestamp = (new Date(item.time).getTime() + '').replace(/\d{3}$/, '');
-	var description = sanitize(item.description);
-	return `<DT><A HREF="${item.href}" ADD_DATE="${timestamp}">${item.title}</A>
-		<DD>${description}
-	`;
-};
+var plist_data = plist.readFileSync(READING_LIST_FILE);
+var reading_list = getReadingList(plist_data);
+var bookmarks = reading_list.map(parseBookmark);
+var html_bookmarks = bookmarks.map(item_formatter).join('\n');
 
-function getReadingList(data) {
-	var reading_list = data.Children.find(function(item) {
-		return item.Title === 'com.apple.ReadingList';
-	});
-	return reading_list ? reading_list.Children : [];
-};
-
-function parseBookmark(bookmark) {
-	return {
-		href: bookmark.URLString,
-		title: bookmark.ReadingListNonSync.Title || bookmark.URIDictionary.title,
-		time: bookmark.ReadingList.DateAdded,
-		description: bookmark.ReadingList.PreviewText || ''
-	}
-};
-
-var reading_list = getReadingList(plist.readFileSync(READING_LIST_FILE)).map(parseBookmark);
-var html = netscape_template(reading_list.map(item_formatter).join('\n'));
-
-fs.outputFile(path.join('dist', 'bookmarks.html'), html);
+fs.outputFile(path.join('dist', 'bookmarks.html'), netscape_template(html_bookmarks));
